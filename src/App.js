fd61985d93dc867e4c8413b77545abb3f5163ccc
khj08 <katy.70.hj@gmail.com>
2023-05-31 22:38:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Header/>
+        <Main/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
